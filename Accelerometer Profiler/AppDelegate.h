efc97d09e83ed45c6bacd6563f66061395ccaf96
Stan <aspyrx@Stanley-Zhangs-MacBook-Pro.local>
2014-07-12 13:26:30 -0400

    Initial Commit

---
+++ Accelerometer Profiler/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Accelerometer Profiler
+//
+//  Created by Stan on 7/12/14.
+//  Copyright (c) 2014 aspyrx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Accelerometer ProfilerTests/Accelerometer_ProfilerTests.m
@@ -0,0 +1,34 @@
+//
+//  Accelerometer_ProfilerTests.m
+//  Accelerometer ProfilerTests
+//
+//  Created by Stan on 7/12/14.
+//  Copyright (c) 2014 aspyrx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Accelerometer_ProfilerTests : XCTestCase
+
+@end
+
+@implementation Accelerometer_ProfilerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
